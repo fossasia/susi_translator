ef99f3a7cb333cf889d768e733d8555202702e48
Nirupon123 <niruponpal@gmail.com>
2026-05-27 11:09:22 +0000

    feat(core): implement abstract translation provider architecture

---
+++ flask/providers/__init__.py
@@ -0,0 +1,9 @@
+from .base import TranslationProvider, TranslationError, ProviderConfigError
+from .registry import ProviderRegistry
+
+__all__ = [
+    "TranslationProvider",
+    "TranslationError",
+    "ProviderConfigError",
+    "ProviderRegistry",
+]
