d177892f39926d9bd5f7cd5f95bad63b856a2f65
Nirupon123 <niruponpal@gmail.com>
2026-06-18 00:52:45 +0000

    added redis backed rate limiting

--- flask/auth/extensions.py
+++ flask/auth/extensions.py
@@ -1,4 +1,6 @@
+import os
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address, storage_uri="memory://")
+_storage_uri = os.environ.get("REDIS_URL", "memory://")
+limiter = Limiter(key_func=get_remote_address, storage_uri=_storage_uri)
